1c0ff9511b767d0f55db7540289fb3c927fd6ae8
Vishesh Kumar <vishesh@visheshk.net>
2012-03-22 19:36:50 +0530

    rsi.c fixed, numerous stupid errors uncovered and removed. Now correct simple moving averages and RSI is computed (for the first few dates which have been extracted.)

--- rsi.c
+++ rsi.c
@@ -97,11 +97,17 @@ float calcrsi(float today[][5], int n){
 	int i = 0;
 	float smova[100][2]; //simple moving average [n][U], [][D]
 	//rs[n] = [n][0]/ [n][1]
-	//rsi[n] = f (rs[n]) = 100 - (100 / (1 + rs[n])) 
+	//rs[n] = f (rs[n]) = 100 - (100 / (1 + rs[n])) 
 	float avgu = 0, avgd = 0;
+	for(; i < 100; i++){
+		smova[i][0] = 0;
+		smova[i][1] = 0;
+	}
+	i = 1;
+//	smova[0][0] = today[0][2]; smova[0][1] = today[0][3]; // initial values, anyway zero
 	for (; i < n; i++){
-		smova[i][0] += today[i][2];
-		smova[i][1] += today[i][3];	
+		smova[i][0] = today[i][2] + smova[i - 1][0];
+		smova[i][1] += today[i][3] + smova[i - 1][1];
 	}
 	i--;
 	smova[i][0] /= n; smova[i][1] /= n;
@@ -110,28 +116,37 @@ float calcrsi(float today[][5], int n){
 	if(smova[i][1] == 0)
 		rs == 100;
 	else{
-		smova[i][0] / smova[i][1];
-		rs += 1;
-		rs = 100 / rs;
-		rs = 100 - rs;
+		rs = smova[i][0] / smova[i][1];
+		rs += 1.0;
+		rs = 100.0 / rs;
+		rs = 100.0 - rs;
 	}
+//	printf("\n\n%f\n\n", rs);
 	today[i][4] = rs;
+	rs = 0;
 	i++;
 //	printf("\n%f\n", rs);
 	while(today[i][0] != 0.000){
-		smova[i][0] += ((today[i][2] / n) - (today[i - n][2]/n));
-		smova[i][0] += ((today[i][3] / n) - (today[i - n][3]/n));
+		smova[i][0] = smova[i - 1][0] + ((today[i][2] / n) - (today[i - n][2]/n));
+		smova[i][1] = smova[i - 1][1] + ((today[i][3] / n) - (today[i - n][3]/n));
 		if(smova[i][1] == 0)
 			rs == 100;
 		else{
-			smova[i][0] / smova[i][1];
-			rs += 1;
+			rs = smova[i][0] / smova[i][1];
+			rs += 1.0;
 			rs = 100 / rs;
 			rs = 100 - rs;
 		}
 		today[i][4] = rs;
+		rs = 0;
 		i++;
 	}
+	int l = 0;
+	printf("\n"); // to print smova's both columns, to check obtained simple moving averages
+	for(; l < 10;  l++){
+		printf("%f\t%f\n", smova[l][0], smova[l][1]);
+	}
+	printf("\n");
 }
 
 int main(){
@@ -145,7 +160,7 @@ int main(){
 	}
 	today[0][2] == 0; today[0][3] == 0;
 	oneday(name, today, day);
-	int n = 2;
+	int n = 3;
 	calcrsi(today, n);	
 	for(i = 0; i < 10; i++){
 		printf("%f\t%f\t%f\t%f\t%f\n", today[i][0], today[i][1], today[i][2], today[i][3], today[i][4]);
